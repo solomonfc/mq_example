ea21acf6ad8135b46c0e5b2d6975ceef1742b3c0
Bruce <solomon.fc@gmail.com>
2018-03-01 11:01:31 +0800

    fix clang hiredis code

--- hiredis/hiredis_loop_pop.c
+++ hiredis/hiredis_loop_pop.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include <sys/timeb.h>
 #include <hiredis/hiredis.h>
 
@@ -11,8 +10,6 @@ long long getSystemTime() {
 
 int main(int argc,char *argv[]){
 	
-
-
 	redisContext *c = redisConnect("127.0.0.1", 7000);
 	if (c == NULL || c->err) {
 		if (c) {
@@ -27,41 +24,28 @@ int main(int argc,char *argv[]){
 
 	reply = redisCommand(c, "AUTH h@HZI7SyC6v23QkL2&BV6^dfe7OUufW3");
 	freeReplyObject(reply);
-	
-	long long loop_start=getSystemTime();
 
 	long long start=getSystemTime();
 	long long end;
 	
 	int i, j;
 	while(i<2000000){
-		
+	
 		reply = redisCommand(c, "RPOP %s", "clist");
-		
-		if(strlen(reply->str)==0){
-			freeReplyObject(reply);	
-			break;
-		}
+		freeReplyObject(reply);
 
-		freeReplyObject(reply);	
-
-		i++;		
-
+		i++;
 		if(j>=10000){
 
 			j=0;
 			end=getSystemTime();			
-			printf("10000 pop time cost: %lld ms\n", end-start);
+			printf("10000 pop time cost(ms): %lld \n", end-start);
 			start=getSystemTime();
 
 		}
 		
 		j++;
-
 	}
-	
-	long long loop_end=getSystemTime();
-	printf("total time: %lld ms\n", loop_end-loop_start);
 	
 	return 0;
 }
